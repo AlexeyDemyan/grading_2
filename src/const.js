188de130d00ee2b43b8d22f9ade56c3db08305ba
Alexey Demyanov <deal110988@him-mail.ch>
2023-04-19 21:10:38 +0200

    filters and sorts in progress

--- src/const.js
+++ src/const.js
@@ -10,4 +10,28 @@ const UpdateType = {
   INIT: 'INIT',
 };
 
+const FilterReason = {
+  ALL: 'for-all',
+  BIRTHDAY: 'for-birthday',
+  BRIDE: 'for-bride',
+  MOTHER: 'for-mother',
+  COLLEAGUE: 'for-colleague',
+  DARLING: 'for-darling',
+};
+
+const FilterColor = {
+  ALL: 'all',
+  RED: 'red',
+  WHITE: 'white',
+  LILAC: 'lilac',
+  YELLOW: 'yellow',
+  PINK: 'pink',
+};
+
+const SortType = {
+  DEFAULT: 'default',
+  PRICE_DOWN: 'price-down',
+  PRICE_UP: 'price-up',
+};
+
 export { ApiCredentials, UpdateType }
---
+++ src/model/filters-model.js
@@ -0,0 +1,15 @@
+import Observable from '../framework/observable.js';
+import { FilterReason, FilterColor } from '../const.js';
+
+export default class FiltersModel extends Observable {
+  #filterReason = FilterReason.ALL;
+  #filterColor = FilterColor.ALL;
+
+  get filterReason() {
+    return this.#filterReason;
+  }
+
+  get filterColor() {
+    return this.#filterColor;
+  }
+}
